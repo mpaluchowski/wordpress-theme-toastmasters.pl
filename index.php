39115fdd96cbaa3274861ddcc7e0070721b8b03e
Michal Paluchowski <michal@paluchowski.com>
2016-04-19 12:05:28 +0200

    Insertig header content

--- index.php
+++ index.php
@@ -10,6 +10,19 @@
 
 <body <?php body_class( 'page-body' ) ?>>
 
+<div class="page-header-container">
+<header class="page-element page-header">
+	<span class="page-header__organization-breadcrumb">
+		<a class="page-header__organization-link" href="http://www.toastmasters.org/">Toastmasters</a>,
+		<a class="page-header__organization-link" href="http://toastmastersd95.org/">District 95</a>
+	</span>
+
+	<h1 class="page-header__site-name"><?php bloginfo( 'name' ) ?></h1>
+
+	<img src="<?php bloginfo( 'template_directory' ) ?>/images/toastmasters-logo.png" alt="Toastmasters International Logo" class="page-header__site-logo">
+</header>
+</div>
+
 </body>
 
 </html>
