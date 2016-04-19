65b4074f5b96e481a1a7afc35d93d1c010f50fd6
Michal Paluchowski <michal@paluchowski.com>
2016-04-19 13:21:06 +0200

    Adding missing fonts

--- functions.php
+++ functions.php
@@ -10,6 +10,20 @@ function toastmasterspl_setup() {
 add_action( 'after_setup_theme', 'toastmasterspl_setup' );
 
 function toastmasterspl_scripts() {
+	wp_enqueue_style(
+		'toastmasterspl-fontawesome',
+		'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css',
+		array(),
+		null
+		);
+
+	wp_enqueue_style(
+		'toastmasterspl-roboto',
+		'https://fonts.googleapis.com/css?family=Roboto:400,400italic,700,700italic&amp;subset=latin,latin-ext',
+		array(),
+		null
+		);
+
 	wp_enqueue_style( 'toastmasterspl-style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'toastmasterspl_scripts' );
