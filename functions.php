1ccc740877b1b777f22a34c5a9a4218374494a85
Michal Paluchowski <michal@paluchowski.com>
2016-04-20 14:33:46 +0200

    Unifying indentation to spaces

--- functions.php
+++ functions.php
@@ -1,61 +1,61 @@
 <?php
 
 function toastmasterspl_setup() {
-	add_theme_support( 'title-tag' );
+    add_theme_support( 'title-tag' );
 
-	add_theme_support( 'custom-logo', array(
-		'height' => 100,
-		'width' => 100,
-		'flex-height' => false,
-		'flex-width' => true,
-	) );
+    add_theme_support( 'custom-logo', array(
+        'height' => 100,
+        'width' => 100,
+        'flex-height' => false,
+        'flex-width' => true,
+    ) );
 
     add_theme_support( 'post-thumbnails' );
     set_post_thumbnail_size( 1000, 9999 );
 
-	register_nav_menus( array(
-		'page-navigation'  => __( 'Primary Page Navigation Menu', 'toastmasterspl' ),
-		'organization-breadcrumb'  => __( 'Organization Breadcrumb Menu', 'toastmasterspl' ),
-	) );
+    register_nav_menus( array(
+        'page-navigation'  => __( 'Primary Page Navigation Menu', 'toastmasterspl' ),
+        'organization-breadcrumb'  => __( 'Organization Breadcrumb Menu', 'toastmasterspl' ),
+    ) );
 
-	add_editor_style( array( 'css/editor-style.css' ) );
+    add_editor_style( array( 'css/editor-style.css' ) );
 }
 add_action( 'after_setup_theme', 'toastmasterspl_setup' );
 
 function toastmasterspl_scripts() {
-	wp_enqueue_style(
-		'toastmasterspl-fontawesome',
-		'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css',
-		array(),
-		null
-		);
+    wp_enqueue_style(
+        'toastmasterspl-fontawesome',
+        'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css',
+        array(),
+        null
+        );
 
-	wp_enqueue_style(
-		'toastmasterspl-roboto',
-		'https://fonts.googleapis.com/css?family=Roboto:400,400italic,700,700italic&amp;subset=latin,latin-ext',
-		array(),
-		null
-		);
+    wp_enqueue_style(
+        'toastmasterspl-roboto',
+        'https://fonts.googleapis.com/css?family=Roboto:400,400italic,700,700italic&amp;subset=latin,latin-ext',
+        array(),
+        null
+        );
 
-	wp_enqueue_style( 'toastmasterspl-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'toastmasterspl-style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'toastmasterspl_scripts' );
 
 function toastmasterspl_the_custom_logo( $class = 'custom-logo' ) {
-	$html = '';
+    $html = '';
 
-	$custom_logo_id = get_theme_mod( 'custom_logo' );
+    $custom_logo_id = get_theme_mod( 'custom_logo' );
 
-	// We have a logo. Logo is go.
-	if ( $custom_logo_id ) {
-		$html = wp_get_attachment_image( $custom_logo_id, 'full', false, array(
-				'class'    => $class,
-				'itemprop' => 'logo',
-			)
-		);
-	}
+    // We have a logo. Logo is go.
+    if ( $custom_logo_id ) {
+        $html = wp_get_attachment_image( $custom_logo_id, 'full', false, array(
+                'class'    => $class,
+                'itemprop' => 'logo',
+            )
+        );
+    }
 
-	echo $html;
+    echo $html;
 }
 
 require get_template_directory() . '/inc/Customizable_Walker_Nav_Menu.php';
--- index.php
+++ index.php
@@ -12,86 +12,86 @@
 
 <div class="page-header-container">
 <header class="page-element page-header">
-	<?php
-		wp_nav_menu( array(
-			'theme_location' => 'organization-breadcrumb',
-			'menu_class' => 'page-header__organization-breadcrumb organization-breadcrumb',
-			'container' => false,
-			'depth' => 1,
-			'walker' => new Customizable_Walker_Nav_Menu(),
-			'items_wrap' => '<span class="%2$s">%3$s</span>',
-			'toastmasterspl_item_wrap' => ['<span class="organization-breadcrumb__item">', '</span>'],
-			'toastmasterspl_link_class' => 'organization-breadcrumb__link',
-		 ) );
-	?>
+    <?php
+        wp_nav_menu( array(
+            'theme_location' => 'organization-breadcrumb',
+            'menu_class' => 'page-header__organization-breadcrumb organization-breadcrumb',
+            'container' => false,
+            'depth' => 1,
+            'walker' => new Customizable_Walker_Nav_Menu(),
+            'items_wrap' => '<span class="%2$s">%3$s</span>',
+            'toastmasterspl_item_wrap' => ['<span class="organization-breadcrumb__item">', '</span>'],
+            'toastmasterspl_link_class' => 'organization-breadcrumb__link',
+         ) );
+    ?>
 
-	<div class="page-header__site-name"><?php bloginfo( 'name' ) ?></div>
+    <div class="page-header__site-name"><?php bloginfo( 'name' ) ?></div>
 
-	<?php toastmasterspl_the_custom_logo( 'page-header__site-logo' ) ?>
+    <?php toastmasterspl_the_custom_logo( 'page-header__site-logo' ) ?>
 </header>
 </div>
 
 <main class="page-element page-content">
-	<div class="page-content__main">
+    <div class="page-content__main">
         <?php
             while ( have_posts() ) : the_post();
         ?>
-		<header class="page-content__article-header article-header<?php if ( has_post_thumbnail() ): ?> article-header--featured-image<?php endif ?>">
+        <header class="page-content__article-header article-header<?php if ( has_post_thumbnail() ): ?> article-header--featured-image<?php endif ?>">
             <?php
                 if ( has_post_thumbnail() ) {
                     the_post_thumbnail( 'post-thumbnail', [ 'class' => 'article-header__image' ] );
                 }
             ?>
             <?php the_title( '<h1 class="article-header__title">', '</h1>' ) ?>
-		</header>
-		<article class="page-content__article-text article-text">
+        </header>
+        <article class="page-content__article-text article-text">
             <?php the_content() ?>
-		</article>
+        </article>
         <?php endwhile; // have_posts() ?>
-	</div>
+    </div>
 
-	<div class="page-content__aside">
-		<aside class="page-content__widget">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id lectus dolor. Fusce ut sagittis lectus. Proin sagittis lectus sed neque hendrerit, vitae commodo magna fermentum.</aside>
-		<aside class="page-content__widget">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id lectus dolor. Fusce ut sagittis lectus. Proin sagittis lectus sed neque hendrerit, vitae commodo magna fermentum.</aside>
-	</div>
+    <div class="page-content__aside">
+        <aside class="page-content__widget">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id lectus dolor. Fusce ut sagittis lectus. Proin sagittis lectus sed neque hendrerit, vitae commodo magna fermentum.</aside>
+        <aside class="page-content__widget">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id lectus dolor. Fusce ut sagittis lectus. Proin sagittis lectus sed neque hendrerit, vitae commodo magna fermentum.</aside>
+    </div>
 </main>
 
 <div class="page-footer-container">
 <footer class="page-element page-footer">
-	<div class="page-footer__social-links social-links">
-		<a href="https://www.facebook.com/ToastmastersPolskaPL/" class="social-links__link"><i class="fa fa-fw fa-facebook"></i><span class="social-links__text">Facebook</span></a>
-		<a href="#" class="social-links__link"><i class="fa fa-fw fa-twitter"></i><span class="social-links__text">Twitter</span></a>
-		<a href="#" class="social-links__link"><i class="fa fa-fw fa-youtube"></i><span class="social-links__text">YouTube</span></a>
-	</div>
+    <div class="page-footer__social-links social-links">
+        <a href="https://www.facebook.com/ToastmastersPolskaPL/" class="social-links__link"><i class="fa fa-fw fa-facebook"></i><span class="social-links__text">Facebook</span></a>
+        <a href="#" class="social-links__link"><i class="fa fa-fw fa-twitter"></i><span class="social-links__text">Twitter</span></a>
+        <a href="#" class="social-links__link"><i class="fa fa-fw fa-youtube"></i><span class="social-links__text">YouTube</span></a>
+    </div>
 
-	<p class="page-footer__copyright">Toastmasters International, the Toastmasters International logo and all other Toastmasters International trademarks and copyrights are the sole property of <a href="http://www.toastmasters.org/" rel="external">Toastmasters International</a>. This <a href="http://www.wordpress.org">WordPress</a> template is freely <a href="https://github.com/mpaluchowski/wordpress-theme-toastmasters.pl">available on <i class="fa fa-github"></i> GitHub</a> under the Apache License, Version 2.0. Make something cool.</p>
+    <p class="page-footer__copyright">Toastmasters International, the Toastmasters International logo and all other Toastmasters International trademarks and copyrights are the sole property of <a href="http://www.toastmasters.org/" rel="external">Toastmasters International</a>. This <a href="http://www.wordpress.org">WordPress</a> template is freely <a href="https://github.com/mpaluchowski/wordpress-theme-toastmasters.pl">available on <i class="fa fa-github"></i> GitHub</a> under the Apache License, Version 2.0. Make something cool.</p>
 </footer>
 </div>
 
 <div class="page-navigation-container" id="page-navigation">
 <nav class="page-element page-navigation">
-	<?php
-		wp_nav_menu( array(
-			'theme_location' => 'page-navigation',
-			'container' => false,
-			'depth' => 1,
-			'items_wrap' => '%3$s',
-			'walker' => new Customizable_Walker_Nav_Menu(),
-			'toastmasterspl_link_class' => 'page-navigation__link',
-			'toastmasterspl_link_class_current' => 'page-navigation__link--current',
-			'toastmasterspl_link_icons' => [
-				1 => 'home'
-			],
-			'toastmasterspl_icon_class' => 'page-navigation__link--icon',
-			'toastmasterspl_text_class' => 'page-navigation__link-text',
-		 ) );
-	?>
+    <?php
+        wp_nav_menu( array(
+            'theme_location' => 'page-navigation',
+            'container' => false,
+            'depth' => 1,
+            'items_wrap' => '%3$s',
+            'walker' => new Customizable_Walker_Nav_Menu(),
+            'toastmasterspl_link_class' => 'page-navigation__link',
+            'toastmasterspl_link_class_current' => 'page-navigation__link--current',
+            'toastmasterspl_link_icons' => [
+                1 => 'home'
+            ],
+            'toastmasterspl_icon_class' => 'page-navigation__link--icon',
+            'toastmasterspl_text_class' => 'page-navigation__link-text',
+         ) );
+    ?>
 </nav>
 </div>
 
 <div class="page-navigation-link-container">
 <div class="page-element page-navigation-link">
-	<a href="#page-navigation" class="page-navigation-link__link"><i class="fa fa-bars"></i></a>
+    <a href="#page-navigation" class="page-navigation-link__link"><i class="fa fa-bars"></i></a>
 </div>
 </div>
 
