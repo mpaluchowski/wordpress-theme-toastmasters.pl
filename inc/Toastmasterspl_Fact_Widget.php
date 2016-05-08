fb70ec2c1a54ac2b5afa04ff207777b0dfd66ab2
Michal Paluchowski <michal@paluchowski.com>
2016-05-08 17:59:06 +0200

    Fixes to the fact widget
    Using title for fact_major, since Wordpress expects widgets to have
    titles.

--- inc/Toastmasterspl_Fact_Widget.php
+++ inc/Toastmasterspl_Fact_Widget.php
@@ -32,7 +32,7 @@ class Toastmasterspl_Fact_Widget extends WP_Widget {
         echo $args['before_widget'];
         ?>
         <div class="fact-widget fact-widget--<?php echo $format ?>">
-            <div class="fact-widget__fact fact-widget__fact-major"><?php echo $instance['fact_major'] ?></div>
+            <div class="fact-widget__fact fact-widget__fact-major"><?php echo $instance['title'] ?></div>
             <?php if ( !empty( $instance['fact_minor'] ) ): ?><div class="fact-widget__fact fact-widget__fact-minor"><?php echo $instance['fact_minor'] ?></div><?php endif; // !empty fact_minor ?>
         </div>
         <?php
@@ -40,20 +40,19 @@ class Toastmasterspl_Fact_Widget extends WP_Widget {
     }
 
     function form( $instance ) {
-        $fact_major = empty( $instance['fact_major'] ) ? '' : esc_attr( $instance['fact_major'] );
+        $fact_major = empty( $instance['title'] ) ? '' : esc_attr( $instance['title'] );
         $fact_minor = empty( $instance['fact_minor'] ) ? '' : esc_attr( $instance['fact_minor'] );
         $format = isset( $instance['format'] ) && array_key_exists( $instance['format'], $this->formats )
             ? $instance['format']
             : self::FORMAT_DEFAULT;
-
         ?>
         <p>
-            <label for="<?php echo esc_attr( $this->get_field_id( 'fact_major' ) ); ?>"><?php _e( 'Major fact:', 'toastmasterspl' ); ?></label>
-            <input id="<?php echo esc_attr( $this->get_field_id( 'fact_major' ) ); ?>" class="widefat" name="<?php echo esc_attr( $this->get_field_name( 'fact_major' ) ); ?>" type="text" value="<?php echo esc_attr( $fact_major ); ?>">
+            <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Major fact:', 'toastmasterspl' ); ?></label>
+            <input id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" class="widefat" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo $fact_major; ?>">
         </p>
         <p>
             <label for="<?php echo esc_attr( $this->get_field_id( 'fact_minor' ) ); ?>"><?php _e( 'Minor fact:', 'toastmasterspl' ); ?></label>
-            <input id="<?php echo esc_attr( $this->get_field_id( 'fact_minor' ) ); ?>" class="widefat" name="<?php echo esc_attr( $this->get_field_name( 'fact_minor' ) ); ?>" type="text" value="<?php echo esc_attr( $fact_minor ); ?>">
+            <input id="<?php echo esc_attr( $this->get_field_id( 'fact_minor' ) ); ?>" class="widefat" name="<?php echo esc_attr( $this->get_field_name( 'fact_minor' ) ); ?>" type="text" value="<?php echo $fact_minor; ?>">
         </p>
         <p>
             <label for="<?php echo esc_attr( $this->get_field_id( 'format' ) ); ?>"><?php _e( 'Background color:', 'toastmasterspl' ); ?></label>
@@ -67,13 +66,13 @@ class Toastmasterspl_Fact_Widget extends WP_Widget {
     }
 
     function update( $new_instance, $old_instance ) {
-        $old_instance['fact_major'] = strip_tags( $new_instance['fact_major'] );
-        $old_instance['fact_minor'] = strip_tags( $new_instance['fact_minor'] );
+        $instance = $old_instance;
+        $instance['title'] = strip_tags( $new_instance['title'] );
+        $instance['fact_minor'] = strip_tags( $new_instance['fact_minor'] );
         if ( array_key_exists( $new_instance['format'], $this->formats ) ) {
-            $old_instance['format'] = $new_instance['format'];
+            $instance['format'] = $new_instance['format'];
         }
-
-        return $old_instance;
+        return $instance;
     }
 
 }
