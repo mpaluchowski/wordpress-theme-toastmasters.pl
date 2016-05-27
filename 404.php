da0a037409858ee7d26a2ef6914aebdcb1c9ddcb
Michal Paluchowski <michal@paluchowski.com>
2016-05-27 08:48:21 +0200

    Initial empty 404 page

---
+++ 404.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html <?php language_attributes() ?>>
+    <meta charset="<?php bloginfo( 'charset' ) ?>">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <?php wp_head() ?>
+</html>
+
+<body>
+
+<?php wp_footer() ?>
+
+</body>
+
+</html>
